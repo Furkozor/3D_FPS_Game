1b5bf7f7a255128749b643cc52fedf70ffaced3b
Furkozor <furkant.k.06@gmail.com>
2024-01-21 17:57:03 +0300

    v1.3
    Pause Menu eklendi ancak Main Menu olmadığı için Main Menu'ye dönülemiyor.

--- 3DFPSGame/Assets/Scripts/MouseLook.cs
+++ 3DFPSGame/Assets/Scripts/MouseLook.cs
@@ -5,27 +5,24 @@ using UnityEngine;
 public class MouseLook : MonoBehaviour
 {
     [Range(50, 500)]
-    public float sens;
+    public float sens; // Sensitivity ayarý için 50 ile 500 arasýnda zorunlu kýlýnmýþ sens deðiþkeni tanýmladýk.
 
-    public Transform body;
+    public Transform body; // Karakterimizi döndürmek için body tanýmladýk
 
-    float xRot = 0f;
+    float xRot = 0f; // Rotasyon için xRot deðiþkeni tanýmladýk;
 
     void Update()
     {
-        float rotX = Input.GetAxisRaw("Mouse X") * sens * Time.deltaTime; // X Rotasyonu oluþturup bu X
-                                                                          // rotasyonunu Mouse'un X
-                                                                          // rotasyonundan alýyoruz
+        float rotX = Input.GetAxisRaw("Mouse X") * sens * Time.deltaTime; // X Rotasyonu oluþturup bu X rotasyonunu
+                                                                          // Mouse'un X rotasyonundan aldýk.
 
-        float rotY = Input.GetAxisRaw("Mouse Y") * sens * Time.deltaTime; // Y Rotasyonu oluþturup bu Y
-                                                                          // rotasyonunu Mouse'un Y
-                                                                          // rotasyonundan alýyoruz
+        float rotY = Input.GetAxisRaw("Mouse Y") * sens * Time.deltaTime; // Y Rotasyonu oluþturup bu Y rotasyonunu
+                                                                          // Mouse'un Y rotasyonundan aldýk.
 
         // --- KAMERAYI YUKARI AÞAÐI DÖNDÜRME --- \\
-        xRot -= rotY; // Kamerayý Y ekseninde ayarlamak için rotasyonu ayarlýyoruz.
-        xRot = Mathf.Clamp(xRot, -90f, 90f); // Kameranýn Y rotasyonunu sýnýrlandýrýyoruz ki 360
-                                             // derece dönmesin
-        transform.localRotation = Quaternion.Euler(xRot, 0f, 0f); // Y rotasyonunu xRot kadar döndürüyoruz
+        xRot -= rotY; // Kamerayý Y ekseninde ayarlamak için rotasyonu ayarladýk.
+        xRot = Mathf.Clamp(xRot, -90f, 90); // Kameranýn Y rotasyonunu -90(tam aþaðý) ile 90(tam yukarý) olarak sýnýrlandýrdýk ki 360 derece dönmesin.
+        transform.localRotation = Quaternion.Euler(xRot, 0f, 0f); // Y rotasyonunu xRot kadar döndür.
 
         // --- KAMERAYI SAÐA SOLA DÖNDÜRME --- \\
         body.Rotate(Vector3.up * rotX);
