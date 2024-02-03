1de1e931fbf0f4ed5938a3e6559ac18e88a1c447
Sundywich <yldrm_kaan@hotmail.com>
2024-02-04 01:52:15 +0300

    Clock of the puzzle
    Started to develop the clock of the perspective puzzle. It is ready as a variable but its not ready as a model.
    
    A 3d model and some more code needed.

--- 3DFPSGame(V2.1)/Assets/Scripts/PerspectivePuzzleScripts/DayAndNightConsole.cs
+++ 3DFPSGame(V2.1)/Assets/Scripts/PerspectivePuzzleScripts/DayAndNightConsole.cs
@@ -6,20 +6,62 @@ using UnityEngine.Experimental.GlobalIllumination;
 public class DayAndNightConsole : MonoBehaviour
 {
     [Header("Sun of the Scene")]
-    public DirectionalLight DirectionalLight;
-    [SerializeField] private float xRotationOfTheSun;
-    [SerializeField] private float yRotationOfTheSun;
+    public Light DirectionalLight;
+    [SerializeField] [Range(1,12)] private int Clock;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        //xRotationOfTheSun = ;
-        //yRotationOfTheSun = ;
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+
+        switch (Clock) // Change the directional light rotation according to the clock
+        {
+            case 1:
+            DirectionalLight.transform.rotation = Quaternion.Euler(15*Clock, -30 , 0);
+                break;
+
+            case 2:
+                DirectionalLight.transform.rotation = Quaternion.Euler(15 * Clock, -30, 0);
+                break;
+
+            case 3:
+                DirectionalLight.transform.rotation = Quaternion.Euler(15 * Clock, -30, 0);
+                break;
+
+            case 4:
+                DirectionalLight.transform.rotation = Quaternion.Euler(69 , -30, 0);
+                break;
+
+            case 5:
+                DirectionalLight.transform.rotation = Quaternion.Euler(15 * Clock, -30, 0);
+                break;
+
+            case 6:
+                DirectionalLight.transform.rotation = Quaternion.Euler(15 * Clock, -30, 0);
+                break;
+
+            case 7:
+                DirectionalLight.transform.rotation = Quaternion.Euler(15 * Clock, -30, 0);
+                break;
+
+            case 8:
+                DirectionalLight.transform.rotation = Quaternion.Euler(15 * Clock, -30, 0);
+                break;
+
+            case 9:
+                DirectionalLight.transform.rotation = Quaternion.Euler(131, -30, 0);
+                break;
+
+            case 10:
+                DirectionalLight.transform.rotation = Quaternion.Euler(15 * Clock, -30, 0);
+                break;
+
+            case 11:
+                DirectionalLight.transform.rotation = Quaternion.Euler(15 * Clock, -30, 0);
+                break;
+
+            case 12:
+                DirectionalLight.transform.rotation = Quaternion.Euler(15 * Clock, -30, 0);
+                break;
+        }
+       
     }
 }
