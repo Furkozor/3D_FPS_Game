ff0e5c0d04aae1f04128c0d6469fe069cd200b23
Sundywich <yldrm_kaan@hotmail.com>
2024-02-09 00:08:47 +0300

    Perspective puzzle finished
    The code part is completely done (for now at least). Waiting for models and arts to make it more visual.

--- 3DFPSGame(V2.1)/Assets/Scripts/PerspectivePuzzleScripts/PasswordConsole.cs
+++ 3DFPSGame(V2.1)/Assets/Scripts/PerspectivePuzzleScripts/PasswordConsole.cs
@@ -2,16 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PasswordConsole : MonoBehaviour
 {
     [Header("The true combination (0031 in default)")]
     [SerializeField] private string TheTrueCombination = "0031";
+    private bool TrueCombinationHasBeenEntered = false;
 
     [Header("UI")]
     public GameObject PasswordMenu;
     private bool ConsoleInRange = false;
     public TextMeshProUGUI PasswordText;
+    public Image PasswordTextBackground;
 
     private void Start()
     {
@@ -36,6 +39,13 @@ public class PasswordConsole : MonoBehaviour
                 PasswordMenu.SetActive(false);
                 Time.timeScale = 1;
             }
+        }
+
+        if(PasswordText.text == "0031") // If the player gives the correct answer
+        {
+            TrueCombinationHasBeenEntered = true;
+            PasswordTextBackground.color = Color.green;
+            Debug.Log("You solved the puzzle");
         }
     }
 
@@ -67,113 +77,157 @@ public class PasswordConsole : MonoBehaviour
     }
 
     #region Number buttons
+
+    public void Number0()
+    {
+        if (!TrueCombinationHasBeenEntered)
+        {
+            if (PasswordText.text.Length < 4)
+            {
+                PasswordText.text += "0";
+            }
+            else if (PasswordText.text.Length == 4)
+            {
+                PasswordText.text = "";
+            }
+        }
+    }
+
     public void Number1()
     {
-        if(PasswordText.text.Length < 4)
+        if (!TrueCombinationHasBeenEntered)
         {
-            PasswordText.text += "1";
-        }
-        else if(PasswordText.text.Length == 4)
-        {
-            PasswordText.text = "";
-        }
+            if (PasswordText.text.Length < 4)
+            {
+                PasswordText.text += "1";
+            }
+            else if (PasswordText.text.Length == 4)
+            {
+                PasswordText.text = "";
+            }
+        }       
     }
 
     public void Number2()
     {
-        if (PasswordText.text.Length < 4)
+        if (!TrueCombinationHasBeenEntered)
         {
-            PasswordText.text += "2";
-        }
-        else if (PasswordText.text.Length == 4)
-        {
-            PasswordText.text = "";
+            if (PasswordText.text.Length < 4)
+            {
+                PasswordText.text += "2";
+            }
+            else if (PasswordText.text.Length == 4)
+            {
+                PasswordText.text = "";
+            }
         }
     }
 
     public void Number3()
     {
-        if (PasswordText.text.Length < 4)
+        if (!TrueCombinationHasBeenEntered)
         {
-            PasswordText.text += "3";
-        }
-        else if (PasswordText.text.Length == 4)
-        {
-            PasswordText.text = "";
+            if (PasswordText.text.Length < 4)
+            {
+                PasswordText.text += "3";
+            }
+            else if (PasswordText.text.Length == 4)
+            {
+                PasswordText.text = "";
+            }
         }
     }
 
     public void Number4()
     {
-        if (PasswordText.text.Length < 4)
+        if (!TrueCombinationHasBeenEntered)
         {
-            PasswordText.text += "4";
-        }
-        else if (PasswordText.text.Length == 4)
-        {
-            PasswordText.text = "";
+            if (PasswordText.text.Length < 4)
+            {
+                PasswordText.text += "4";
+            }
+            else if (PasswordText.text.Length == 4)
+            {
+                PasswordText.text = "";
+            }
         }
     }
 
     public void Number5()
     {
-        if (PasswordText.text.Length < 4)
+        if (!TrueCombinationHasBeenEntered)
         {
-            PasswordText.text += "5";
-        }
-        else if (PasswordText.text.Length == 4)
-        {
-            PasswordText.text = "";
+            if (PasswordText.text.Length < 4)
+            {
+                PasswordText.text += "5";
+            }
+            else if (PasswordText.text.Length == 4)
+            {
+                PasswordText.text = "";
+            }
         }
     }
 
     public void Number6()
     {
-        if (PasswordText.text.Length < 4)
+        if (!TrueCombinationHasBeenEntered)
         {
-            PasswordText.text += "6";
-        }
-        else if (PasswordText.text.Length == 4)
-        {
-            PasswordText.text = "";
+            if (PasswordText.text.Length < 4)
+            {
+                PasswordText.text += "6";
+            }
+            else if (PasswordText.text.Length == 4)
+            {
+                PasswordText.text = "";
+            }
         }
     }
 
     public void Number7()
     {
-        if (PasswordText.text.Length < 4)
+        if (!TrueCombinationHasBeenEntered)
         {
-            PasswordText.text += "7";
-        }
-        else if (PasswordText.text.Length == 4)
-        {
-            PasswordText.text = "";
+            if (PasswordText.text.Length < 4)
+            {
+                PasswordText.text += "7";
+            }
+            else if (PasswordText.text.Length == 4)
+            {
+                PasswordText.text = "";
+            }
         }
     }
 
     public void Number8()
     {
-        if (PasswordText.text.Length < 4)
+        if (!TrueCombinationHasBeenEntered)
         {
-            PasswordText.text += "8";
-        }
-        else if (PasswordText.text.Length == 4)
-        {
-            PasswordText.text = "";
+            if (PasswordText.text.Length < 4)
+            {
+                PasswordText.text += "8";
+            }
+            else if (PasswordText.text.Length == 4)
+            {
+                PasswordText.text = "";
+            }
         }
     }
 
     public void Number9()
     {
-        if (PasswordText.text.Length < 4)
+        if (!TrueCombinationHasBeenEntered)
         {
-            PasswordText.text += "9";
-        }
-        else if (PasswordText.text.Length == 4)
-        {
-            PasswordText.text = "";
+            if (PasswordText.text.Length < 4)
+            {
+                PasswordText.text += "9";
+            }
+            else if (PasswordText.text.Length == 4)
+            {
+                PasswordText.text = "";
+            }
         }
     }
+
     #endregion
 
     #endregion
