7f68d4dc51b12add504de857e993fe21f3685482
Furkna <erbilginfurkan@gmail.com>
2024-02-14 18:40:10 +0300

    PuzzleParadoxV2.2 (Scale Puzzle)
    -- Ana menüye Settings Paneli eklendi
    
    -- Mouse Sensitivity ayarı eklendi
    
    -- Pause menüdeki ana menü butonunun doğru çalışmaması fixlendi.
    
    --Yeni Level eklendi.

---
+++ 3DFPSGame(V2.2)/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool paused = false;
+    public GameObject pauseMenuCanvas;
+
+    private void Start()
+    {
+        Time.timeScale = 1f;
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(paused)
+                Continue();
+            else
+                Pause();
+        }
+    }
+
+    public void Pause()
+    {
+        pauseMenuCanvas.SetActive(true); // Pause ekranýmýzý aktif ediyoruz.
+        Time.timeScale = 0f; // Oyunun zaman akýþýný kapatýyoruz.
+        paused = true;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    public void Continue()
+    {
+        pauseMenuCanvas.SetActive(false); // Pause ekranýmýzý deaktif ediyoruz.
+        Time.timeScale = 1f; // Oyunun zaman akýþýný açýyoruz.
+        paused = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void MainMenuButton()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
